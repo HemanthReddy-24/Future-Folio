2a9c55e26ed8903a67d0fcb306ca7b16acf07ea8
Hemanth Reddy <kalakotahemanthreddy24@gmail.com>
2024-08-19 23:04:00 +0530

    Created Hero Section Left

--- static/js/script.js
+++ static/js/script.js
@@ -8,24 +8,26 @@ const signupLoginLink = formPopup.querySelectorAll(".bottom-link a");
 
 // Show mobile menu
 hamburgerBtn.addEventListener("click", () => {
-    navbarMenu.classList.toggle("show-menu");
+  navbarMenu.classList.toggle("show-menu");
 });
 
 // Hide mobile menu
-hideMenuBtn.addEventListener("click", () =>  hamburgerBtn.click());
+hideMenuBtn.addEventListener("click", () => hamburgerBtn.click());
 
 // Show login popup
 showPopupBtn.addEventListener("click", () => {
-    document.body.classList.toggle("show-popup");
+  document.body.classList.toggle("show-popup");
 });
 
 // Hide login popup
 hidePopupBtn.addEventListener("click", () => showPopupBtn.click());
 
 // Show or hide signup form
-signupLoginLink.forEach(link => {
-    link.addEventListener("click", (e) => {
-        e.preventDefault();
-        formPopup.classList[link.id === 'signup-link' ? 'add' : 'remove']("show-signup");
-    });
+signupLoginLink.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    formPopup.classList[link.id === "signup-link" ? "add" : "remove"](
+      "show-signup"
+    );
+  });
 });
